cf4e887e04aa4c20cc73a4173a81ce93719a2809
handosensei@gmail.com <handosensei@gmail.com>
2022-09-17 02:32:07 +0200

    Save all traits

--- src/Repository/TraitTypeRepository.php
+++ src/Repository/TraitTypeRepository.php
@@ -2,14 +2,14 @@
 
 namespace App\Repository;
 
-use App\Entity\Attribute;
+use App\Entity\TraitType;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-class AttributeRepository extends ServiceEntityRepository
+class TraitTypeRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Attribute::class);
+        parent::__construct($registry, TraitType::class);
     }
 }
