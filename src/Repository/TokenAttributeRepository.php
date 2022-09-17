37423342ce96db16df177365f5a03912b02cd20c
Raldo <raldo.chea@gmail.com>
2022-09-17 15:31:31 +0200

    APP : Token Index

--- src/Repository/TokenAttributeRepository.php
+++ src/Repository/TokenAttributeRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Token;
 use App\Entity\TokenAttribute;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -11,5 +12,20 @@ class TokenAttributeRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, TokenAttribute::class);
+    }
+
+    /**
+     * @param Token $token
+     * @return mixed
+     */
+    public function findByToken(Token $token)
+    {
+        return $this->createQueryBuilder('ta')
+            ->leftjoin('ta.attribute', 'a')
+            ->leftJoin('a.traitType', 'tt')
+            ->where('ta.token = :token')
+            ->orderBy('tt.name')
+            ->setParameter('token', $token)
+            ->getQuery()->getResult();
     }
 }
