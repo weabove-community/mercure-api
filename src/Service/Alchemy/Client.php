cf121a425bfe885ac89993cf300c9283cff47181
handosensei <handosensei@gmail.com>
2023-02-21 00:16:00 +0100

    WeAbove : check only weAbove collection

--- src/Service/Alchemy/Client.php
+++ src/Service/Alchemy/Client.php
@@ -18,14 +18,37 @@ class Client
         $uri = sprintf('https://eth-mainnet.g.alchemy.com/nft/v2/%s/getNFTs', self::API_KEY);
 
         $options = [
-            'query' => [
+            'query' => $this->buildQuery([
                 'contractAddresses' => $smartContracts,
                 'owner' => $owner,
                 'withMetadata' => 'true'
-            ]
+            ])
         ];
 
         $client = new GuzzleClient();
         return $client->request('GET', $uri, $options);
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     * @throws \Exception
+     */
+    private function buildQuery(array $options)
+    {
+        $query = [];
+        foreach ($options as $key => $value) {
+            if (is_array($value)) {
+                foreach($value as $element) {
+                    if (is_array($element)) {
+                        throw new \Exception('l option %s a pour valeur un array imbriqué');
+                    }
+                    $query[] = sprintf('%s[]=%s', $key, $element);
+                }
+                continue;
+            }
+            $query[] = sprintf('%s=%s', $key, $value);
+        }
+        return implode('&', $query);
     }
 }
